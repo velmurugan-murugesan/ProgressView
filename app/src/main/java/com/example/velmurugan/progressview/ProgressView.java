d1fe3251d3fbc5d8447b59ef831ae6495d4c6326
velmurugan35 <velmurugan.murugesan@gmail.com>
2018-03-15 23:07:05 +0530

    Created ProgressView

---
+++ app/src/main/java/com/example/velmurugan/progressview/ProgressView.java
@@ -0,0 +1,24 @@
+package com.example.velmurugan.progressview;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by Velmurugan on 3/15/2018.
+ */
+
+public class ProgressView extends View {
+    int canvasWidth,canvasHeight;
+    public ProgressView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        canvasWidth = MeasureSpec.getSize(widthMeasureSpec);
+        canvasHeight = MeasureSpec.getSize(heightMeasureSpec);
+    }
+}
