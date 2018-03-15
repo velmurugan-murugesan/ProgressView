dac530908b3fdcc51e5f5946776afb6949e2c676
velmurugan35 <velmurugan.murugesan@gmail.com>
2018-03-15 23:12:01 +0530

    add paint

--- app/src/main/java/com/example/velmurugan/progressview/ProgressView.java
+++ app/src/main/java/com/example/velmurugan/progressview/ProgressView.java
@@ -2,6 +2,7 @@ package com.example.velmurugan.progressview;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
@@ -11,6 +12,7 @@ import android.view.View;
  */
 
 public class ProgressView extends View {
+    Paint paint;
     int canvasWidth,canvasHeight;
     public ProgressView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
